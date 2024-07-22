3c26d3ab4323c1f8be7b4bc5cd2708870aeea293
Muzammal Anwar <muzammalanwar556@gmail.com>
2024-07-22 13:43:46 +0200

    render and show more Pokemons feature implemented

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
+let pokemons = {}
+let index = 20;
+
 const BASE_URL = 'https://pokeapi.co/api/v2/';
 
 function init() {
-    getPokemon('pokemon?limit=10&offset=0', 0, 10);
+    getPokemon(50, 0);
 }
 
-async function getPokemon(path, start, end) {
+async function getPokemon(limit, offset) {
+    let path = `pokemon?limit=${limit}&offset=${offset}`
     let response = await fetch(BASE_URL + path + ".json");
-    return responseAsText = await response.json(), console.log(responseAsText);
+    let responseAsText = await response.json();
+    pokemons = responseAsText.results;
+    console.log(pokemons)
+    render()
 }
 
 function render() {
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-
+    let inputContent = document.getElementById('pokeCardContainer');
+    inputContent.innerHTML = '';
+    for (let i = 0; i < (pokemons.length > 20 ? index : pokemons.length); i++) {
+        inputContent.innerHTML += /*HTML*/`
+            <div class="card" id="pokemon${i}">
+                <h1>${i + 1} ${pokemons[i].name}</h1>
+            </div>
+        `;
     }
+    showMoreImg()
+}
+
+function showMoreImg() {
+    let inputContent = document.getElementById('showMoreIcon');
+    inputContent.innerHTML = /*HTML*/`
+        <img id="showMoreIconImg" class="showMoreIcon" onclick="showMorePokemons()" src="./img/showMoreIcon.png">
+    `;
+}
+
+function showMorePokemons() {
+    index > 20 ? (index = pokemons.length, hideElement('morePokemons', 'd-none')) : (index += 20);
+    render();
+}
+
+function hideElement(id, x) {
+    document.getElementById(id).classList.add(x);
+}
+
+function showElement(id, x) {
+    document.getElementById(id).classList.remove(x);
 }
